73f0c6eb91f79f5b582915c25bfa55fa1ccf4000
nicoboss <nico@bosshome.ch>
2021-10-21 11:59:25 +0200

    Batch/Shell Skripte entfernt da alles per docker-compose oder runonsave gebuildet wird

--- client/build.cmd
+++
@@ -1,7 +0,0 @@
-call npm install
-rmdir /Q /S dist
-mkdir dist
-copy node_modules\\jiff-mpc\\lib\\jiff-client.js dist
-call npx tsc app.ts
-call npx webpack
-del app.js
--- client/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install
-rm -rf dist
-mkdir dist
-cp node_modules/jiff-mpc/lib/jiff-client.js dist
-npx tsc app.ts
-npx webpack
-rm -f app.js
