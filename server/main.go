d76ba79b5adc783795163b6f9afdd57075e8bc11
nicoboss <nico@bosshome.ch>
2021-10-25 21:25:39 +0200

    Begonnen das initialization socketProtocol zu implementieren

--- server/main.go
+++ server/main.go
@@ -9,7 +9,94 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type SocketMaps struct {
+	SocketId      map[string]map[int]*websocket.Conn
+	ComputationId map[*websocket.Conn]string
+	PartyId       map[*websocket.Conn]int
+}
+
+type InputMessage struct {
+	SocketProtocol string
+	Data           string
+}
+
+type InputMessageDataInitialization struct {
+	Computation_id string
+	Party_count    int
+	Public_key     string
+}
+
+type OutputMessage struct {
+	SocketProtocol string
+	Data           string
+}
+
+type InnerOutputMessageError struct {
+	ErrorProtocol string
+	Error         string
+}
+
+type InitializePartyError struct {
+	success bool
+	error   string
+}
+
+type InitializePartyMsg struct {
+	party_id    int
+	party_count int
+	public_keys string
+}
+
+func toJSON(obj interface{}) string {
+	jsonObj, err := json.Marshal(obj)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return string(jsonObj)
+}
+
+func toObj(str []byte, obj interface{}) {
+	err := json.Unmarshal(str, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
 var upgrader = websocket.Upgrader{}
+var socketMaps = new(SocketMaps)
+
+func initializeParty(computation_id string, party_id int, party_count int, _s1 bool) (bool, InitializePartyMsg) {
+	var message = InitializePartyMsg{party_id: 0, party_count: 0, public_keys: ""}
+	return true, message
+}
+
+func initialization(data string, party_id int, socket *websocket.Conn) {
+
+	var inputData = &InputMessageDataInitialization{}
+	toObj([]byte(data), inputData)
+
+	success, message := initializeParty(inputData.Computation_id, party_id, inputData.Party_count, false)
+
+	if socketMaps.SocketId[inputData.Computation_id] == nil {
+		socketMaps.SocketId[inputData.Computation_id] = make(map[int]*websocket.Conn)
+	}
+
+	if success {
+		socketMaps.SocketId[inputData.Computation_id][message.party_id] = socket
+		socketMaps.ComputationId[socket] = inputData.Computation_id
+		socketMaps.PartyId[socket] = message.party_id
+
+		party_id = message.party_id
+		outputMessageObj := &OutputMessage{SocketProtocol: "initialization", Data: toJSON(message)}
+		socket.WriteJSON(outputMessageObj)
+		log.Printf("Sent: %s", toJSON(outputMessageObj))
+	} else {
+		innerOutputMessageErrorObj := &InnerOutputMessageError{ErrorProtocol: "initialization", Error: toJSON(message)}
+		outputMessageObj := &OutputMessage{SocketProtocol: "error", Data: toJSON(innerOutputMessageErrorObj)}
+		socket.WriteJSON(outputMessageObj)
+		log.Printf("Sent: %s", toJSON(outputMessageObj))
+	}
+}
 
 func socketHandler(w http.ResponseWriter, r *http.Request) {
 	// Upgrade der Verbindung von HTTP auf websocket
@@ -29,13 +116,18 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		log.Printf("Received: %s", data)
-		msg := map[string]string{}
-		json.Unmarshal([]byte(data), &msg)
-		log.Print(msg)
 
-		switch socketProtocol := msg["socketProtocol"]; socketProtocol {
+		var inputMessage = &InputMessage{}
+		toObj(data, inputMessage)
+		log.Printf("Unmarshaled: %s", inputMessage.Data)
+
+		log.Print(inputMessage)
+		party_id := 0
+
+		switch socketProtocol := inputMessage.SocketProtocol; socketProtocol {
 		case "initialization":
 			fmt.Println("initialization")
+			initialization(inputMessage.Data, party_id, conn)
 		case "share":
 			fmt.Println("share")
 		case "open":
@@ -51,7 +143,7 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 		case "free":
 			fmt.Println("free")
 		default:
-			fmt.Printf("Unimplementiertes socketProtocol: %s.\n", socketProtocol)
+			fmt.Println("Unimplementiertes socketProtocol:", socketProtocol)
 		}
 
 		err = conn.WriteMessage(messageType, data)
@@ -63,6 +155,11 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.Println("Server Started!")
+	socketMaps.SocketId = make(map[string]map[int]*websocket.Conn)
+	socketMaps.SocketId = make(map[string]map[int]*websocket.Conn)
+	socketMaps.ComputationId = make(map[*websocket.Conn]string)
+	socketMaps.PartyId = make(map[*websocket.Conn]int)
 	http.HandleFunc("/", socketHandler)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
