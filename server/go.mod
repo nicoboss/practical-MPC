9b5f1790b4a646152f04565168dd871345e6effb
nicoboss <nico@bosshome.ch>
2021-10-27 20:11:48 +0200

    Initialisierungsschritt (exclusive Mailbox) fertig implementiert

--- server/go.mod
+++ server/go.mod
@@ -3,3 +3,5 @@ module PracticalMPC/Server
 go 1.17
 
 require github.com/gorilla/websocket v1.4.2
+
+require github.com/miguelsandro/curve25519-go v0.0.0-20171107140843-564b467e915d // indirect
--- server/main.go
+++ server/main.go
@@ -1,18 +1,36 @@
+//Golang code lose basierend auf JIFF Server JavaScript Code
+
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/websocket"
+	"github.com/miguelsandro/curve25519-go/axlsign"
+	//"github.com/miguelsandro/curve25519-go/axlsign"
+	//"github.com/jamesruan/sodium" //choco install pkgconfiglite
 )
 
+// speichert Zustände der Berechnung
+type ComputationMaps struct {
+	ClientIds   map[string][]string           // { computation_id -> [ party1_id, party2_id, ...] } for only registered/initialized clients
+	SpareIds    map[string][]bool             // { computation_id -> <interval object> }
+	MaxCount    map[string]int                // { computation_id -> <max number of parties allowed> }
+	Keys        map[string]map[string][]uint8 // { computation_id -> { party_id -> <public_key> } }
+	SecretKeys  map[string][]uint8            // { computation_id -> <privateKey> }
+	FreeParties map[string]map[string]bool    // { computation_id -> { id of every free party -> true } }
+}
+
 type SocketMaps struct {
-	SocketId      map[string]map[int]*websocket.Conn
+	SocketId      map[string]map[string]*websocket.Conn
 	ComputationId map[*websocket.Conn]string
-	PartyId       map[*websocket.Conn]int
+	PartyId       map[*websocket.Conn]string
 }
 
 type InputMessage struct {
@@ -36,16 +54,26 @@ type InnerOutputMessageError struct {
 	Error         string
 }
 
-type InitializePartyError struct {
-	success bool
-	error   string
+type InitializePartyMsg struct {
+	party_id    string
+	party_count int
+	public_keys map[string]string
 }
 
-type InitializePartyMsg struct {
-	party_id    int
-	party_count int
-	public_keys string
+type InnerCryptoMap struct {
+	shares []string
+	values []string
 }
+
+type KeymapToSend struct {
+	public_keys map[string]string
+}
+
+var upgrader = websocket.Upgrader{}
+var computationMaps = new(ComputationMaps)
+var socketMaps = new(SocketMaps)
+var mailbox = make(map[string]map[string][]string)                    // { computation_id -> { party_id -> linked_list<[ message1, message2, ... ]> } }
+var cryptoMap = make(map[string]map[string]map[string]InnerCryptoMap) // { computation_id -> { op_id -> { party_id -> { 'shares': [ numeric shares for this party ], 'values': <any non-secret value(s) for this party> } } } }
 
 func toJSON(obj interface{}) string {
 	jsonObj, err := json.Marshal(obj)
@@ -62,23 +90,151 @@ func toObj(str []byte, obj interface{}) {
 	}
 }
 
-var upgrader = websocket.Upgrader{}
-var socketMaps = new(SocketMaps)
+func getPartyIdInt(party_id string) int {
+	party_id_int, err := strconv.Atoi(party_id)
+	if err != nil {
+		log.Fatal("party_id ist keine Ganzzahl")
+	}
+	return party_id_int
+}
 
-func initializeParty(computation_id string, party_id int, party_count int, _s1 bool) (bool, InitializePartyMsg) {
-	var message = InitializePartyMsg{party_id: 0, party_count: 0, public_keys: ""}
+func contains(s []string, str string) bool {
+	for _, item := range s {
+		if item == str {
+			return true
+		}
+	}
+	return false
+}
+
+func assertAvailablePRNG() {
+	buf := make([]byte, 1)
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		panic(fmt.Sprintf("Kein kryptographisch sicheren Zufallsgenerator vorhanden: crypto/rand: Read() failed with %#v", err))
+	}
+}
+
+func GenerateRandomBytes(n int) []uint8 {
+	b := make([]uint8, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func parseKey(keyString string) []byte {
+	var arr []byte
+	toObj([]byte(keyString), &arr)
+	return arr
+}
+
+func dumpKey(key []byte) string {
+	return ("[" + string(key) + "]")
+}
+
+func initComputation(computation_id string, party_id string, party_count int) {
+	if computationMaps.ClientIds[computation_id] == nil {
+		computationMaps.ClientIds[computation_id] = make([]string, party_count)
+		computationMaps.MaxCount[computation_id] = party_count
+		computationMaps.FreeParties[computation_id] = make(map[string]bool)
+		computationMaps.Keys[computation_id] = make(map[string][]uint8)
+		socketMaps.SocketId[computation_id] = make(map[string]*websocket.Conn)
+		mailbox[computation_id] = make(map[string][]string)
+		cryptoMap[computation_id] = make(map[string]map[string]InnerCryptoMap)
+	}
+
+	if !(contains(computationMaps.ClientIds[computation_id], party_id)) {
+		computationMaps.ClientIds[computation_id] = append(computationMaps.ClientIds[computation_id], party_id)
+	}
+}
+
+func storeAndSendPublicKey(computation_id string, party_id string, public_key string) map[string]string {
+	// Öffendlicher Schlüssel speichern
+	var tmp = computationMaps.Keys[computation_id]
+	if _, ok := tmp["s1"]; !ok { // Public/Private Schlüsselpaar generieren falls diese noch nicht existieren
+		var genkey = axlsign.GenerateKeyPair(GenerateRandomBytes(32)) // Generieren des Schlüsselpaars
+		computationMaps.SecretKeys[computation_id] = genkey.PrivateKey
+		tmp["s1"] = genkey.PublicKey
+	}
+
+	if party_id != "s1" {
+		tmp[party_id] = parseKey(public_key)
+	}
+
+	// Sammeln und formatieren der Schlüssel
+	var keymap_to_send = new(KeymapToSend)
+	keymap_to_send.public_keys = make(map[string]string)
+	for key, _ := range tmp {
+		if val, ok := computationMaps.Keys[computation_id][key]; !ok {
+			keymap_to_send.public_keys[key] = dumpKey(val)
+		}
+	}
+	var broadcast_message = toJSON(keymap_to_send)
+
+	// Öffentlicher Schlüssel an alle zuvor verbundenen Parteien ausser der Partei welche dieses Update verursacht hat senden
+	for _, party := range computationMaps.ClientIds[computation_id] {
+		if party != party_id {
+			//safe_emit("public_keys", broadcast_message, computation_id, party)
+			log.Println("public_keys", broadcast_message, computation_id, party)
+		}
+	}
+
+	return keymap_to_send.public_keys
+}
+
+// Initialisierung einer Partei. Rückgabe: Initialisierungsnachricht mit der Partei-ID oder eine Fehlermeldung
+func initializeParty(computation_id string, party_id string, public_key string, party_count int, _s1 bool) (bool, InitializePartyMsg) {
+
+	log.Println("Server inizialisiert mit ", computation_id, "-", party_id, " #", party_count, "::", _s1)
+
+	if _s1 != true && party_id == "s1" {
+		log.Fatal("party_id s1 ist für den Server reserviert und darf nicht von einem Client verwendet werden!")
+	}
+
+	// Liste aller noch verfügbaren Partei erstellen
+	if computationMaps.SpareIds[computation_id] == nil {
+		computationMaps.SpareIds[computation_id] = make([]bool, party_count)
+	}
+
+	if party_id != "" {
+		if party_id != "s1" && !computationMaps.SpareIds[computation_id][getPartyIdInt(party_id)] == false {
+			log.Fatal("party_id existiert schon")
+		}
+	} else { // generate einer freien party_id
+		var currentSpareIds = computationMaps.SpareIds[computation_id]
+		for i := range currentSpareIds {
+			if currentSpareIds[i] == false {
+				party_id = string(i)
+				break
+			}
+		}
+	}
+
+	if party_id != "s1" {
+		computationMaps.SpareIds[computation_id][getPartyIdInt(party_id)] = true
+	}
+
+	// Initialisierung der Berechnung und definieren aller noch undefinierten Objekten.
+	initComputation(computation_id, party_id, party_count)
+
+	// Initialisierungsnachricht für den Client erstellen
+	keymap_to_send := storeAndSendPublicKey(computation_id, party_id, public_key)
+
+	var message = InitializePartyMsg{party_id: party_id, party_count: 0, public_keys: keymap_to_send}
 	return true, message
 }
 
-func initialization(data string, party_id int, socket *websocket.Conn) {
+func initialization(data string, party_id string, socket *websocket.Conn) {
 
 	var inputData = &InputMessageDataInitialization{}
 	toObj([]byte(data), inputData)
 
-	success, message := initializeParty(inputData.Computation_id, party_id, inputData.Party_count, false)
+	success, message := initializeParty(inputData.Computation_id, party_id, inputData.Public_key, inputData.Party_count, false)
 
 	if socketMaps.SocketId[inputData.Computation_id] == nil {
-		socketMaps.SocketId[inputData.Computation_id] = make(map[int]*websocket.Conn)
+		socketMaps.SocketId[inputData.Computation_id] = make(map[string]*websocket.Conn)
 	}
 
 	if success {
@@ -87,10 +243,19 @@ func initialization(data string, party_id int, socket *websocket.Conn) {
 		socketMaps.PartyId[socket] = message.party_id
 
 		party_id = message.party_id
-		outputMessageObj := &OutputMessage{SocketProtocol: "initialization", Data: toJSON(message)}
+		outputMessageObj := &OutputMessage{
+			SocketProtocol: "initialization",
+			Data:           toJSON(message),
+		}
 		socket.WriteJSON(outputMessageObj)
 		log.Printf("Sent: %s", toJSON(outputMessageObj))
+
+		// Now that party is connected and has the needed public keys,
+		// send the mailbox with pending messages to the party.
+		// jiff.resend_mailbox(computation_id, party_id)
 	} else {
+		// Change error to its own protocol type since ws does not support error messages natively
+		/* Messages sent over socket.io are now under the label 'data' while previously used protocols are sent under 'socketProtocol' */
 		innerOutputMessageErrorObj := &InnerOutputMessageError{ErrorProtocol: "initialization", Error: toJSON(message)}
 		outputMessageObj := &OutputMessage{SocketProtocol: "error", Data: toJSON(innerOutputMessageErrorObj)}
 		socket.WriteJSON(outputMessageObj)
@@ -122,7 +287,7 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Unmarshaled: %s", inputMessage.Data)
 
 		log.Print(inputMessage)
-		party_id := 0
+		party_id := "party_id"
 
 		switch socketProtocol := inputMessage.SocketProtocol; socketProtocol {
 		case "initialization":
@@ -156,10 +321,10 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("Server Started!")
-	socketMaps.SocketId = make(map[string]map[int]*websocket.Conn)
-	socketMaps.SocketId = make(map[string]map[int]*websocket.Conn)
+	assertAvailablePRNG()
+	socketMaps.SocketId = make(map[string]map[string]*websocket.Conn)
 	socketMaps.ComputationId = make(map[*websocket.Conn]string)
-	socketMaps.PartyId = make(map[*websocket.Conn]int)
+	socketMaps.PartyId = make(map[*websocket.Conn]string)
 	http.HandleFunc("/", socketHandler)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
