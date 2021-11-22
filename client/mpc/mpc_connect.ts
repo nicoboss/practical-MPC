a29a17a0448e470cdf3b91dae70da3361884680a
nicoboss <nico@bosshome.ch>
2021-11-22 22:23:34 +0100

    Grundgerüst für MPC ohne Server als Crypto Provider

--- client/mpc/mpc_connect.ts
+++ client/mpc/mpc_connect.ts
@@ -8,7 +8,9 @@ var saved_instance;
 // Lose basierend auf https://github.com/multiparty/jiff/tree/master/demos/sum
 exports.mpc_connect = function (hostname: string, computation_id: string, options: any) {
   var opt = Object.assign({}, options);
-  opt.crypto_provider = true;
+  var use_crypto_provider: boolean;
+  use_crypto_provider = (<HTMLInputElement>document.getElementById("use_crypto_provider")).checked;
+  opt.crypto_provider = use_crypto_provider
   saved_instance = new JIFFClient(hostname, computation_id, opt);
   saved_instance.apply_extension(jiff_websockets, opt);
   exports.saved_instance = saved_instance;
