cce9c5fdc35ef574307fc60e84304945acb508ad
nicoboss <nico@bosshome.ch>
2021-12-24 17:42:18 +0100

    Zustandsänderungen des Verbindungsbuttons verbessert

--- client/components/connect-button.ts
+++ client/components/connect-button.ts
@@ -13,12 +13,14 @@ exports.connect_button = function (app: any) {
         connectButtonText: "Verbinden",
         isConnecting: false,
         isConnected: false,
+        isError: false,
       }
     },
     methods: {
       connectButtonClick() {
         var _this = this;
         this.isConnecting = true;
+        this.isError = false;
         this.connectButtonEnabled = false;
         this.connectButtonText = "Warten auf Teilnehmer...";
         let computation_id = (<HTMLInputElement>document.getElementById("computation_id")).value;
@@ -31,12 +33,18 @@ exports.connect_button = function (app: any) {
           var options: any = { party_count: party_count};
           options.onError = function (_ : any, error : string): any {
             logger.log(error, logger.LogType.ERROR);
+            (<HTMLDivElement>document.getElementById("calculate_box")).classList.add("disable-controls");
+            (<HTMLDivElement>document.getElementById("settings_box")).classList.remove("disable-controls");
+            (<HTMLDivElement>document.getElementById("settings_box")).scrollIntoView();
             _this.connectButtonText = "Verbinden erneut versuchen";
+            _this.isConnected = false;
             _this.connectButtonEnabled = true;
+            _this.isError = true;
             _this.isConnecting = false;
           };
           options.onConnect = function () {
             _this.isConnected = true;
+            _this.isError = false;
             _this.isConnecting = false;
             _this.connectButtonText = "Verbunden";
             logger.log("Alle Parteien verbunden!", logger.LogType.INFO);
@@ -75,7 +83,7 @@ exports.connect_button = function (app: any) {
       }
     },
     template: `
-      <button id="connect_button" v-on:click="connectButtonClick()" :disabled="!connectButtonEnabled" :class="{ 'button-x-connected': isConnected }">
+      <button id="connect_button" v-on:click="connectButtonClick()" :disabled="!connectButtonEnabled" :class="{ 'button-x-connecting': isConnecting, 'button-x-connected': isConnected, 'button-x-error': isError }">
         <span>{{connectButtonText}}</span>
         <span v-if="isConnecting" class="loader">
           <span class="loader-box"></span>
