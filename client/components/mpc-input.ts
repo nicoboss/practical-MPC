97a4243aba929eedc36dbaf0c755600c41864080
nicoboss <nico@bosshome.ch>
2022-01-02 14:58:02 +0100

    Infotext zu Berechnungsarten hinzugefügt

--- client/components/mpc-input.ts
+++ client/components/mpc-input.ts
@@ -21,6 +21,7 @@ exports.mpc_input = function (app: any) {
       </tabs>
       <tab-panels v-model="selectedTab" :animate="true">
         <tab-panel val="Summe">
+          <h3>Summe der Eingaben aller Teilnehmer</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="client_input_sum">Summand:</label>
             <input type="number" id="client_input_sum" min="0" max="100" value="7"/>
@@ -29,6 +30,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Multiplikation">
+          <h3>Produkt der Eingaben aller Teilnehmer</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="client_input_multi">Faktor:</label>
             <input type="number" id="client_input_multi" min="0" max="100" value="7"/><br/>
@@ -37,6 +39,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Division">
+          <h3>Division, erster Wert ist Dividend, alle weiteren Eingaben sind Divisoren</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="client_input_div">Dividend/Divisor:</label>
             <input type="number" id="client_input_div" min="0" max="100" value="7"/><br/>
@@ -45,8 +48,8 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Valentine">
+          <h3>Liebst du die anderen Teilnehmer?</h3>
           <div class="command-div one-liner-box d-flex">
-            <label class="mtb-auto">Liebst du die anderen Teilnehmer?</label><br/>
             <div class="d-flex">
               <input class="mtb-auto" type="radio" id="ValentineYes" name="IsValentine" checked="true"/>
               <label class="mtb-auto" for="IsValentine">Ja</label><br/>
@@ -60,6 +63,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Abstimmung">
+          <h3>Stimmabgabe für einen Kanditaten</h3>
           <div class="command-div d-flex">
             <div class="box no_margin">
               <label class="mtb-auto" for="vote_person_security_checks">Sicherheitschecks</label><input class="mtb-auto" type="checkbox" id="vote_person_security_checks" checked="true"/><br/><br/>
@@ -73,6 +77,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Wahl">
+          <h3>Bewertung der Grafikkarten</h3>
           <div class="command-div d-flex">
             <div class="box no_margin">
               <table>
@@ -91,6 +96,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Threshold">
+          <h3>Zählt alle Eingaben die über einem Schwellenwert (Threshold) liegen</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="threshold_input">Threshold</label>
             <input type="number" id="threshold_input" min="0" max="100" value="50"/>
@@ -102,6 +108,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="SumThreshold">
+          <h3>Prüft ob die Summe aller Eingaben über einem Schwellenwert liegen</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="threshold_input_sum">Sum Threshold</label>
             <input type="number" id="threshold_input_sum" min="0" max="100" value="50"/>
@@ -113,6 +120,7 @@ exports.mpc_input = function (app: any) {
           </div>
         </tab-panel>
         <tab-panel val="Standardabweichung">
+          <h3>Berechnet die Standardabweichung aller Eingaben</h3>
           <div class="command-div one-liner-box d-flex">
             <label class="mtb-auto" for="client_input_standard_deviation">Wert:</label>
             <input type="number" id="client_input_standard_deviation" min="0" max="100" value="7"/>
